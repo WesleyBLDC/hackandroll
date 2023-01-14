801b0a3a042a206989c6651e4f40486643f9536a
WesleyBLDC <wezzdc@gmail.com>
2023-01-14 22:36:02 +0800

    added ternary operator

--- hackyrolly/app/page.tsx
+++ hackyrolly/app/page.tsx
@@ -5,7 +5,6 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import results from "../pages/api/result.json";
 
-
 export default function Home() {
   return (
     <div>
@@ -15,28 +14,34 @@ export default function Home() {
           placeholder="Find item..."
           className=" w-11/12 md:w-8/12 xl:w-3/4 h-auto p-3 rounded-3xl bg-white"
         />
-        <button className="btn btn-secondary absolute right-20 rounded-3xl bg-sky-500 border-transparent hover:bg-sky-500 hover:border-transparent">Search</button>
+        <button className="btn btn-secondary absolute right-20 rounded-3xl bg-sky-500 border-transparent hover:bg-sky-500 hover:border-transparent">
+          Search
+        </button>
       </div>
 
-<div className="flex justify-center">
-<div className="grid grid-cols-3 gap-3"> {/* p-3  pl-20 pr-7*/}
-
-        {results.messages.map((item) => (
-          <SmallCard
-          
-            key={item.id}
-            from={item.from || "NA"}
-            message={item.text.toString()}
-            date={item.date.toString()}
-            text={item.text.toString()}
-            from_id={item.from_id || "NA"}
-          />
-   
-        ))}
-  
+      <div className="flex justify-center">
+        <div className="grid grid-cols-3 gap-3">
+          {" "}
+          {/* p-3  pl-20 pr-7*/}
+          {results.messages.map((item) => (
+            <SmallCard
+              key={item.id}
+              from={item.from || "NA"}
+              message={item.text.toString()}
+              date={item.date.toString()}
+              text={item.text.toString()}
+              from_id={item.from_id || "NA"}
+              status={
+                item.text.toString().includes("found")
+                  ? "FOUND"
+                  : item.text.toString().includes("lost")
+                  ? "LOST"
+                  : ""
+              }
+            />
+          ))}
+        </div>
       </div>
-</div>
-      
     </div>
   );
 }
--- hackyrolly/components/SmallCard.tsx
+++ hackyrolly/components/SmallCard.tsx
@@ -1,37 +1,44 @@
 import React from "react";
 
-interface CreateStudentProps {
+interface CreateCardProps {
   from: string;
   message: string;
   text: string;
   date: string;
   from_id: string;
+  status: string;
 }
 
-function SmallCard({ from, message, text, date, from_id }: CreateStudentProps) {
+function SmallCard({
+  from,
+  message,
+  text,
+  date,
+  from_id,
+  status,
+}: CreateCardProps) {
   const dateObj = new Date(date);
   const year = dateObj.getFullYear();
-const month = dateObj.getMonth();
-const date2 = dateObj.getDate();
-date = date2 + "." + month + "." + year;
-
+  const month = dateObj.getMonth();
+  const date2 = dateObj.getDate();
+  date = date2 + "." + month + "." + year;
 
   return (
-    <div className="card w-96 h-96 bg-zinc-100 text-black border border-yellow-700 shadow-xl overflow-auto co"> {/* bg-black*/}
+    <div className="card w-96 h-96 bg-zinc-100 text-black border border-yellow-700 shadow-xl overflow-auto co">
+      {" "}
+      {/* bg-black*/}
       <div className="card-body">
-       
         <h2 className="card-title">
-         
-        <a href = "'https://t.me/' + {from}">{from}</a> {/* {from}*/}
-          
-          <div className="badge badge-secondary  bg-sky-500 border-transparent hover:bg-sky-500 hover:border-transparent">LOST</div>
+          <a href="'https://t.me/' + {from}">{from}</a> {/* {from}*/}
+          <div className="badge badge-secondary  bg-sky-500 border-transparent">
+            {status}
+          </div>
         </h2>
 
         <div>
-        <div className="badge badge-outline">{date}</div>
-
+          <div className="badge badge-outline">{date}</div>
         </div>
-        <p >
+        <p>
           {message} {text}
         </p>
         <div className="card-actions justify-end">
