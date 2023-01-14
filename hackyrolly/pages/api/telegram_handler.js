2c6db729e0cc748ff596f05d3284ef2f5a56221e
darrentde <darrent2599@gmail.com>
2023-01-14 20:54:58 +0800

    Done telegram bot

--- hackyrolly/pages/api/telegram_handler.js
+++ hackyrolly/pages/api/telegram_handler.js
@@ -1,20 +1,37 @@
 /* eslint-disable import/no-anonymous-default-export */
 export default async (req, res) => {
   const tgbot = process.env.NEXT_TELEGRAM_TOKEN;
-  if (req.method === 'POST') {
-    const message =
-      'Initializing group chat';
+
+  if (req.body.message.text === '/start') {
+    // Send reply
+    const message = 'Initializing group chat';
     const ret = await fetch(
       `https://api.telegram.org/bot${tgbot}/sendMessage?chat_id=${req.body.message.chat.id}&text=${message}&parse_mode=HTML`
     );
-    const data = await fetch(
-      `https://api.telegram.org/bot${tgbot}/getHistory?chat_id=${req.body.message.chat.id}&parse_mode=JSON`);
-    console.log(data);
-    // console.log("Yes");
-      // const test = await fetch(
-    //   `https://api.telegram.org/bot${tgbot}/sendMessage?chat_id=${req.body.message.chat.id}&text=${data}&parse_mode=HTML`
-    // );
+  } else {
+    console.log(req.body.message.text);
   }
+
+
+    // const data = await fetch(
+    //   'https://api.telegram.org/bot5894983812:AAEtr5-6Z2s4tQFJhrBnDO_5iNbPtxqOTio/getUpdates'
+    // );
+
+  // }
+  // if (req.method === 'POST') {
+  //   const message =
+  //     'Initializing group chat';
+  //   const ret = await fetch(
+  //     `https://api.telegram.org/bot${tgbot}/sendMessage?chat_id=${req.body.message.chat.id}&text=${message}&parse_mode=HTML`
+  //   );
+  //   const data = await fetch(
+  //     `https://api.telegram.org/bot${tgbot}/getHistory?chat_id=${res}&parse_mode=JSON`);
+  //   console.log(data);
+  //   // console.log("Yes");
+  //     // const test = await fetch(
+  //   //   `https://api.telegram.org/bot${tgbot}/sendMessage?chat_id=${req.body.message.chat.id}&text=${data}&parse_mode=HTML`
+  //   // );
+  // }
 
   // if (req.body.message.text === '/help') {
   //   const message =
